145c278c7dfb6efbd83c9fa0ce8cbdc8362c059b
Shahrukh Ijaz <ijaz@AhmedQasim-Desktop.arbisoft.com>
2018-08-17 18:13:12 +0500

    first commit

--- entry_point.py
+++ entry_point.py
@@ -1,5 +1,5 @@
 from scrapy.cmdline import execute
 
-execute(['scrapy', 'crawl', 'SheegoSpider', '-o', 'sheegoItemFile2.json'])
+execute(['scrapy', 'crawl', 'Sheego_Spider', '-o', 'sheegoItemFile2.json'])
 # execute(['scrapy', 'crawl', 'quotes'])
 # execute(['scrapy', 'shell', 'https://www.sheego.de/'])
